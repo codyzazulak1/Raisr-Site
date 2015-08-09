b6ce6fbe8ccf3c8167a34ee63d990971f0c1dfd1
codyzazulak1 <codyzazulak1@gmail.com>
2015-08-09 21:56:13 +0000

    downloaded jquery

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('form').ajaxChimp({
     callback: function(response) {
+      $('#myForm').hide();
+      $('.checkmark').show();
       $('form .result').text(response.msg);
     }
   });
